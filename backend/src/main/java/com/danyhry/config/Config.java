bbf0c5d594386a9cc48ec169a96295515f1e0368
Danigri-source <dankagrigorev@gmail.com>
2022-04-24 14:49:46 +0300

    init

--- backend/src/main/java/com/danyhry/config/Config.java
+++ backend/src/main/java/com/danyhry/config/Config.java
@@ -3,6 +3,8 @@ package com.danyhry.config;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -10,15 +12,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class Config {
+
+    private final Environment environment;
+
+    public Config(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     public DataSource datasource() {
         return DataSourceBuilder.create()
-                .driverClassName("org.postgresql.Driver")
-                .url("jdbc:postgresql://localhost:5432/diploma")
-                .username("postgres")
-                .password("postgres")
+                .driverClassName(environment.getProperty("spring.datasource.driver-class"))
+                .url(environment.getProperty("spring.datasource.url"))
+                .username(environment.getProperty("spring.datasource.username"))
+                .password(environment.getProperty("spring.datasource.password"))
                 .build();
     }
 
